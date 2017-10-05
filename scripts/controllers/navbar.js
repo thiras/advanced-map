bb469373efdcd6f25d93e3740baa436833f3e68b
Dılo Sürücü <berxudar@gmail.com>
2017-10-05 10:42:12 +0300

    ilçe mahle yol kapı geojon ile arama eklendi

--- scripts/controllers/navbar.js
+++ scripts/controllers/navbar.js
@@ -1,10 +1,9 @@
-app.controller("navbar",function($scope,$accordion,$timeout,$mdDialog,$rootScope) {
+app.controller("navbar", function ($scope, $accordion, $timeout, $mdDialog, $rootScope) {
 
-    $scope.menuJSON =$accordion;
+    $scope.menuJSON = $accordion;
     /*  menu acma ayar bas*/
     $scope.toggleLeft = buildToggler('left');
     $scope.toggleRight = buildToggler('right');
-
 
 
     function buildToggler(componentId) {
@@ -21,11 +20,11 @@ app.controller("navbar",function($scope,$accordion,$timeout,$mdDialog,$rootScope
         var i;
 
         for (i = 0; i < acc.length; i++) {
-            acc[i].onclick = function() {
+            acc[i].onclick = function () {
 
                 this.classList.toggle("active");
                 var panel = this.nextElementSibling;
-                if (panel.style.maxHeight){
+                if (panel.style.maxHeight) {
                     panel.style.maxHeight = null;
                 } else {
                     panel.style.maxHeight = panel.scrollHeight + "px";
@@ -35,16 +34,13 @@ app.controller("navbar",function($scope,$accordion,$timeout,$mdDialog,$rootScope
     });
 
 
+    $scope.factoryDesign = function (fn) {
 
-    $scope.factoryDesign=function (fn) {
-
-       $scope[fn["fonk"]]();
+        $scope[fn["fonk"]]();
     }
 
 
-
-
-    $scope.menuContact=function () {
+    $scope.menuContact = function () {
 
         alert = $mdDialog.alert({
             title: 'İletişim',
@@ -53,19 +49,17 @@ app.controller("navbar",function($scope,$accordion,$timeout,$mdDialog,$rootScope
         });
 
         $mdDialog
-            .show( alert )
-            .finally(function() {
+            .show(alert)
+            .finally(function () {
                 alert = undefined;
             });
     }
 
 
+    $scope.findAddress = function (event) {
 
 
-    $scope.findAddress=function (event) {
-
-
-       // $scope.toggleLeft();
+        // $scope.toggleLeft();
 
         $mdDialog.show({
             controller: navbarCtrl,
@@ -84,36 +78,72 @@ app.controller("navbar",function($scope,$accordion,$timeout,$mdDialog,$rootScope
     }
 
 
-
-    $scope.ulasim=function () {
+    $scope.ulasim = function () {
 
         alert("ulaşım methodu")
     }
 
 
-
-    function navbarCtrl($scope,$sahtejson){
-
-
-        console.log($sahtejson)
+    function navbarCtrl($scope, $sahtejson) {
 
 
-        $scope.cancel=function () {
+        $scope.il = $sahtejson.il;
+        $scope.ilce = $sahtejson.ilce;
+        $scope.mahalle = $sahtejson.mahalle;
+        $scope.yol = $sahtejson.yol;
+        $scope.numarataj = $sahtejson.numarataj;
+        $scope.filterIlce = {};
+        $scope.filterMahalle = {};
+        $scope.isActiveIlce = false;
+        $scope.isActiveMahalle = false;
 
+        $scope.cancel = function () {
             $mdDialog.cancel();
-        }
+        };
 
-
-        $scope.chng=function (a) {
-
-          $scope.ilce=a;
+        $scope.changeIlce = function (ilid) {
+            debugger;
+            $scope.filterIlce = {};
+            for (i in $scope.il) {
+                if (ilid == $scope.il[i].id) {
+                    var il = L.geoJSON($scope.il[i].geojson, {
+                        style: {color: "#ff0000"}
+                    }).bindPopup($scope.il[i].label).addTo($rootScope.leaflet);
+                    var ilbbox = il.getBounds();
+                    $rootScope.leaflet.fitBounds(ilbbox);
+                    $scope.isActiveIlce = true;
+                }
+            }
+            for (i in $scope.ilce) {
+                if (ilid == $scope.ilce[i].ilid) {
+                    $scope.filterIlce[i] = $scope.ilce[i];
+                }
+            }
+        };
+        $scope.changeMahelle = function (ilceid) {
+            debugger;
+            $scope.filterMahalle = {};
+            for (i in $scope.ilce) {
+                if (ilceid == $scope.ilce[i].id) {
+                    var il = L.geoJSON($scope.ilce[i].geojson, {
+                        style: {color: "#ff0000"}
+                    }).bindPopup($scope.ilce[i].label).addTo($rootScope.leaflet);
+                    var ilbbox = il.getBounds();
+                    $rootScope.leaflet.fitBounds(ilbbox);
+                    $scope.isActiveMahalle = true;
+                }
+            }
+            $scope.mahalle = $sahtejson.mahalle;
+            for (i in $scope.mahalle) {
+                if (ilid == $scope.mahalle[i].ilid) {
+                    $scope.filterMahalle[i] = $scope.mahalle[i];
+                }
+            }
 
         }
 
 
     }
-
-
 
 
 });
