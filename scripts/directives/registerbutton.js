0aadbc1d1b84d9b4be7410335c79e569d499b32e
Dılo Sürücü <berxudar@gmail.com>
2017-10-25 16:29:21 +0300

    register buttonu directivi düzeltem  yapıldı

--- scripts/directives/registerbutton.js
+++ scripts/directives/registerbutton.js
@@ -1,18 +1,8 @@
-
 app.directive("registerbutton",function ($http) {
 
     return {
-
-
-
         link:function ($scope,$el,$attr) {
-
-
-
             $el.bind("click",function () {
-
-
-
             })
         }
     }
