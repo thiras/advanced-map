16e3992fbe58c6afc1e18a4f1a3c61d37acd3757
Dılo Sürücü <berxudar@gmail.com>
2017-10-26 15:28:03 +0300

    singletion paterni mdLanguageye eklendi

--- scripts/directives/mdlanguage.js
+++ scripts/directives/mdlanguage.js
@@ -2,20 +2,25 @@ app.directive("mdLanguage", function ($rootScope) {
     return {
 
         restrict: "E",
-        template: ' <md-button ng-click="$mdOpenMenu()" aria-label="Google Hangout" class="md-fab md-raised md-mini"><md-tooltip md-direction="left">{{lang.general.languages}}</md-tooltip>{{langFlag}}<!--<md-icon md-svg-src="img/icons/{{langFlag}}" style="margin-top: 0;" class="mat-icon material-icons accordion-icons ng-binding" role="img" aria-hidden="true">{{langFlag}}</md-icon>--></md-button>',
+        template: ' <md-button aria-label="Google Hangout" class="md-fab md-raised md-mini"><md-tooltip md-direction="left">{{lang.general.languages}}</md-tooltip>{{langFlag}}</md-button>',
         link: function ($scope, $el, $attr) {
             let $supportLang = ["TR", "EN", "AR"];
             $scope.langFlag = navigator.languages[1].toUpperCase();
 
             let indis = $supportLang.indexOf($scope.langFlag);
             let yenisayi = $supportLang.indexOf($scope.langFlag);
+            let singletion=false;
             $el.on("click", function () {
-
 
 
                 if (yenisayi >= $supportLang.length) {
 
                     yenisayi = 0;
+                }
+                if(!singletion){
+
+                    yenisayi+=1;
+                    singletion=true;
                 }
 
                 $scope.langFlag = $supportLang[yenisayi];
