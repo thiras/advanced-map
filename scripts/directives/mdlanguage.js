0e1387851ef6df7d5652bb764a29814d9becca21
Dılo Sürücü <berxudar@gmail.com>
2017-10-26 15:22:04 +0300

    md-language de ki dillerin click olayı ile arayüz değişimi yapıldı dil ile render kaldı

--- scripts/directives/mdlanguage.js
+++ scripts/directives/mdlanguage.js
@@ -1,24 +1,26 @@
-app.directive("mdLanguage",function ($rootScope) {
-    return{
+app.directive("mdLanguage", function ($rootScope) {
+    return {
 
-        restrict:"E",
-        template:' <md-button ng-click="$mdOpenMenu()" aria-label="Google Hangout" class="md-fab md-raised md-mini"><md-tooltip md-direction="left">{{lang.general.moduls}}</md-tooltip>{{langFlag}}<!--<md-icon md-svg-src="img/icons/{{langFlag}}" style="margin-top: 0;" class="mat-icon material-icons accordion-icons ng-binding" role="img" aria-hidden="true">{{langFlag}}</md-icon>--></md-button>',
-        link:function ($scope,$el,$attr) {
+        restrict: "E",
+        template: ' <md-button ng-click="$mdOpenMenu()" aria-label="Google Hangout" class="md-fab md-raised md-mini"><md-tooltip md-direction="left">{{lang.general.languages}}</md-tooltip>{{langFlag}}<!--<md-icon md-svg-src="img/icons/{{langFlag}}" style="margin-top: 0;" class="mat-icon material-icons accordion-icons ng-binding" role="img" aria-hidden="true">{{langFlag}}</md-icon>--></md-button>',
+        link: function ($scope, $el, $attr) {
+            let $supportLang = ["TR", "EN", "AR"];
+            $scope.langFlag = navigator.languages[1].toUpperCase();
 
-            $scope.langFlag="TR";
-            var durum=0;
-            $el.on("click",function () {
+            let indis = $supportLang.indexOf($scope.langFlag);
+            let yenisayi = $supportLang.indexOf($scope.langFlag);
+            $el.on("click", function () {
 
-              if(durum==true){
-                  $scope.langFlag="TR";
 
-              }
-               else{
 
-                  $scope.langFlag="EN";
+                if (yenisayi >= $supportLang.length) {
 
-              }
-                durum=!durum;
+                    yenisayi = 0;
+                }
+
+                $scope.langFlag = $supportLang[yenisayi];
+
+                yenisayi += 1;
 
             });
 
