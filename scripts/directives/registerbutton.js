0ed0664a41d006a0888906dc4dd4b434048707ab
Dılo Sürücü <berxudar@gmail.com>
2017-10-27 14:30:09 +0300

    register login directivleri yazıldı

--- scripts/directives/registerbutton.js
+++ scripts/directives/registerbutton.js
@@ -3,6 +3,10 @@ app.directive("registerbutton",function ($http) {
     return {
         link:function ($scope,$el,$attr) {
             $el.bind("click",function () {
+
+
+                alert(JSON.stringify($scope.user));
+
             })
         }
     }
