05d577882d86a1fd52a08860978dc07efc8f0dc9
Dılo Sürücü <berxudar@gmail.com>
2017-10-30 10:45:50 +0300

    debugger kaldırıldı mdLanguage

--- scripts/directives/mdlanguage.js
+++ scripts/directives/mdlanguage.js
@@ -5,18 +5,18 @@ app.directive("mdLanguage", function ($rootScope, $changelang, $timeout, $accord
         template: ' <md-button ng-if="activeLanguage" aria-label="Google Hangout" class="md-fab md-raised md-mini"><md-tooltip md-direction="left">{{lang.general.languages}}</md-tooltip>{{langFlag}}</md-button>',
         link: function ($scope, $el, $attr) {
 
-                var $supportLang = ["en",$rootScope.browserLangugae];
+            var $supportLang = ["en", $rootScope.browserLangugae];
             $scope.langFlag = $supportLang[0];
             var indis = true;
             $scope.activeLanguage = true;
-            if($rootScope.browserLangugae=="en"){
+            if ($rootScope.browserLangugae == "en") {
                 $scope.activeLanguage = false;
             }
             $el.on("click", function () {
-                debugger;
-                indis=!indis;
-                var a = indis ? 1:0;
-                $scope.langFlag=$supportLang[a];
+
+                indis = !indis;
+                var a = indis ? 1 : 0;
+                $scope.langFlag = $supportLang[a];
 
                 $changelang.change($scope.langFlag).then(function (a) {
                     return a;
@@ -25,9 +25,9 @@ app.directive("mdLanguage", function ($rootScope, $changelang, $timeout, $accord
                     $accordion.json = $accordion.refreshAccordion(b.data);
                     $rootScope.$broadcast("updateAcordion", $accordion.json);
                     $rootScope.accordion = $accordion.json;
-                    var indis2=!indis;
-                    var b = indis2 ? 1:0;
-                    $scope.langFlag=$supportLang[b];
+                    var indis2 = !indis;
+                    var b = indis2 ? 1 : 0;
+                    $scope.langFlag = $supportLang[b];
                 });
 
 
