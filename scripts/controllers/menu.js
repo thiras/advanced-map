c89db6bf3112759f32ebde5ea43ea28e0d467312
Dılo Sürücü <berxudar@gmail.com>
2017-11-15 12:31:29 +0300

    print ile harita yazdırıcıya gönderildi sağ menu ile

--- scripts/controllers/menu.js
+++ scripts/controllers/menu.js
@@ -1,15 +1,24 @@
-app.controller("menuRightClick", function ($scope, $window,$timeout) {
+app.controller("menuRightClick", function ($scope, $window,$timeout,$mdMenu) {
 
     $scope.print = function () {
 
         let answer =confirm("Ekran yazdırılsın mı");
-        document.querySelector("#map").style.zIndex = 100;
-        answer == 1 ? $window.print() : "";
 
-        $timeout(function () {
-            document.querySelector("#map").style.zIndex = 1;
+       if(answer==1){
+           document.querySelector("#map").style.zIndex = 100;
 
-        })
+           $mdMenu.hide();
+
+
+           $timeout(function () {
+               $window.print();
+               document.querySelector("#map").style.zIndex = 1;
+
+           },100);
+
+       }
+
+
 
     }
 
